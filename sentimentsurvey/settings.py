6f0205e4fce2f3920ee59cce1a1f107aee5fc83a
Milos Svana <milos.svana@gmail.com>
2024-05-05 10:51:07 +0200

    Fix CSRF

--- sentimentsurvey/settings.py
+++ sentimentsurvey/settings.py
@@ -122,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CSRF_TRUSTED_ORIGINS = ["survey.swarm.svana.name"]
